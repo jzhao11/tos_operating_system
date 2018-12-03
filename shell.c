0c239cb0d3ca0d3d9198f25c3ec33b4327948790
jzhao11 <1258339346@qq.com>
2018-12-03 03:13:18 -0800

    new commands added into shell

--- shell.c
+++ shell.c
@@ -15,21 +15,20 @@ int compare_string(char* str1, char* str2) {
 	return *str1 == 0 && *str2 == 0;
 }
 
+// print help info
 void print_help(int window_id){
-	wm_print(window_id, "Welcome to TOS Shell!\n");
-	wm_print(window_id, "Need help?\n");
-	wm_print(window_id, "Below you could find some information that might be helpful.\n");
-	wm_print(window_id, "help -- Print out the supported commands in this shell.\n");
+	wm_print(window_id, "Need some help?\n");
+	wm_print(window_id, "Here you can find information that may be helpful.\n");
+	wm_print(window_id, "help -- Print out all the supported commands.\n");
 	wm_print(window_id, "cls -- Clear the screen.\n");
 	wm_print(window_id, "shell -- Launch another shell.\n");
 	wm_print(window_id, "pong -- Launch the PONG game.\n");
-	wm_print(window_id, "echo <str> -- Echo the string <str> to the screen\n");
+	wm_print(window_id, "echo <str> -- Echo the given string to screen.\n");
 	wm_print(window_id, "ps -- Print out the process table.\n");
-	wm_print(window_id, "history -- Print out all the commands that have been typed.\n");
-	wm_print(window_id, "!<number> -- Repeat the command with the given number.\n");
-	wm_print(window_id, "about -- Print out the information about author.\n");
+	wm_print(window_id, "history -- Print out all the typed commands.\n");
+	wm_print(window_id, "!<number> -- Repeat command with the given index.\n");
+	wm_print(window_id, "about -- Print out author's information.\n");
 }
-
 char* parse_command(int window_id) {
 	last = (last + 1) % MAX_CMD;
 	int index = 0;
@@ -39,7 +38,9 @@ char* parse_command(int window_id) {
 			wm_print(window_id, "%c", ch);
 			continue;
 		} else if (ch == 0x08) {
-			commands[last][--index] = '\0';
+			if (index > 0) {
+				commands[last][--index] = '\0';
+			}
 			wm_print(window_id, "\b");
 		} else {
 			wm_print(window_id, "%c", ch);
@@ -56,22 +57,62 @@ char* parse_command(int window_id) {
 	}
 }
 
+// repeat the command
+void repeat_command(int window_id, char* cmd) {
+	// calculate the index after "!"
+	int index = 0;
+	while (*cmd >= '0' && *cmd <= '9') {
+		index = index * 10 + (*cmd - '0');
+		cmd++;
+	}
+
+	// if the index is within bound, execute the command
+	// if the index is out of bound, prompt an error msg
+	if (index <= last) {
+		wm_print(window_id, "%s\n", commands[index]);
+		execute_command(window_id, commands[index]);
+	} else {
+		wm_print(window_id, "Invalid index!\n");
+	}
+}
+
+// execute the command
 void execute_command(int window_id, char* cmd) {
-	//wm_print(window_id, "get command: %s ", cmd);
-	//wm_print(window_id, "last: %d ", last);
-	if (compare_string(cmd, "pong")) {
-		start_pong();
+	if (compare_string(cmd, "help")) {
+		print_help(window_id);
 	} else if (compare_string(cmd, "cls")) {
 		wm_clear(window_id);
+	} else if (compare_string(cmd, "pong")) {
+		start_pong();
+	} else if (compare_string(cmd, "about")) {
+		print_about(window_id);
+	} else if (compare_string(cmd, "shell")) {
+		start_shell();
+	} else if (cmd[0] == '!') {
+		repeat_command(window_id, cmd + 1);
+	} else if (cmd[0] != 0) {
+		wm_print(window_id, "%s Command not found!\n", cmd);
 	}
+}
+
+// print author's info
+void print_about(int window_id) {
+	wm_print(window_id, "***************************************\n");
+	wm_print(window_id, "*  Hello! This is TOS shell for fun!  *\n");
+	wm_print(window_id, "*  Author:  Jianfei (Christian) Zhao  *\n");
+	wm_print(window_id, "*  Contact: christianz0991@gmail.com  *\n");
+	wm_print(window_id, "*  Intro:   The author is seeking an  *\n");
+	wm_print(window_id, "*           summer internship.        *\n");
+	wm_print(window_id, "*  Hobby:   Non-Mickey-Mouse Courses  *\n");
+	wm_print(window_id, "***************************************\n");
 }
 
 // entry point of shell
 void start_shell() {
 	int window_id = wm_create(10, 3, 50, 17);
 	last = -1;
-	wm_print(window_id, "Hello! This is TOS Shell!\n");
-	
+	print_about(window_id);
+
 	while (1) {
 		char* cmd = parse_command(window_id);
 		execute_command(window_id, cmd);
