205f46f2a104eba6008061aec7d6620c3588aefc
jzhao11 <1258339346@qq.com>
2018-12-14 20:55:27 -0800

    configuration 1 solved

--- train.c
+++ train.c
@@ -76,20 +76,87 @@ void change_train_direction() {
 	comm_train(command, "\0", 0);
 }
 
-void config1() {
+void handle_config1(int is_zamboni) {
+	change_switch('6', 'R');
+	change_train_speed('4');
+	while (retrieve_contact("8")) {}
+	change_train_speed('0');
+	change_switch('6', 'G');
+	change_switch('7', 'G');
+	change_train_direction();
+	change_train_speed('4');
+	while (!retrieve_contact("12")) {}
+	change_switch('7', 'R');
+	change_train_speed('0');
+	change_train_direction();
+
+	// pick up wagon
+	if (is_zamboni) {
+		while (!retrieve_contact("10")) {}
+	}
+	change_train_speed('4');
+	change_switch('8', 'R');
+	while (!retrieve_contact("13")) {}
+	change_switch('7', 'G');
+	change_train_speed('0');
+	change_train_direction();
+	change_train_speed('4');
 	
+	while (!retrieve_contact("12") || retrieve_contact("11")) {}
+	change_switch('8', 'G');
+	change_train_speed('0');
+	change_train_direction();
+
+	while (!retrieve_contact("10")) {}
+	change_switch('5', 'R');
+	change_switch('6', 'R');
+	change_train_speed('4');
+	while (!retrieve_contact("7") || retrieve_contact("9")) {}
+	change_train_speed('0');
+	change_train_direction();
+	change_train_speed('4');
+	while (!retrieve_contact("8") || retrieve_contact("7")) {}
+	change_train_speed('0');
+	wm_print(window_id, "Back home.\n");
+	while (1) {}
 }
 
-void config2() {
-
+void handle_config2(int is_zamboni) {
+	change_train_speed('4');
 }
 
-void config3() {
-
+void handle_config3(int is_zamboni) {
+	change_train_speed('4');
 }
 
-void config4() {
+void handle_config4(int is_zamboni) {
+	change_train_speed('4');
+}
 
+void handle_config(int is_zamboni) {
+	if (retrieve_contact("8") && retrieve_contact("11")) {
+		wm_print(window_id, "Configuration 1.\n");
+		handle_config1(is_zamboni);
+	} else if (retrieve_contact("2") && retrieve_contact("12")) {
+		wm_print(window_id, "Configuration 2.\n");
+		handle_config2(is_zamboni);
+	} else if (retrieve_contact("2") && retrieve_contact("11")) {
+		wm_print(window_id, "Configuration 3.\n");
+		handle_config3(is_zamboni);
+	} else if (retrieve_contact("5") && retrieve_contact("12")) {
+		wm_print(window_id, "Configuration 4.\n");
+		handle_config4(is_zamboni);
+	}
+}
+
+int detect_zamboni() {
+	for (int i = 0; i < 20; ++i) {
+		if (retrieve_contact("7")) {
+			wm_print(window_id, "Zamboni found.\n");
+			return 1;
+		}
+	}
+	return 0;
 }
 
 void train_process(PROCESS self, PARAM param) {
@@ -99,24 +166,8 @@ void train_process(PROCESS self, PARAM param) {
 	change_switch('5', 'G');
 	change_switch('8', 'G');
 	change_switch('9', 'R');
-
-	for (int i = 0; i < 200; ++i) {
-		if (retrieve_contact("3") || retrieve_contact("7") || retrieve_contact("14")) {
-			wm_print(window_id, "zamboni found\n");
-			break;
-		}
-	}
-
-	if (retrieve_contact("8") && retrieve_contact("11")) {
-		wm_print(window_id, "configuration 1\n");
-	} else if (retrieve_contact("2") && retrieve_contact("12")) {
-		wm_print(window_id, "configuration 2\n");
-	} else if (retrieve_contact("2") && retrieve_contact("11")) {
-		wm_print(window_id, "configuration 3\n");
-	} else if (retrieve_contact("5") && retrieve_contact("12")) {
-		wm_print(window_id, "configuration 4\n");
-	}
-	change_train_speed('4');
+	int is_zamboni = detect_zamboni();
+	handle_config(is_zamboni);
 	become_zombie();
 }
 
@@ -124,3 +175,21 @@ void init_train() {
 	create_process(train_process, 5, 0, "Train Process");
 	resign();
 }
+
+/*
+// pick up wagon
+	while (retrieve_contact("12")) {}
+	sleep(50);
+	change_train_speed('0');
+	change_train_direction();
+	change_train_speed('4');
+	while (retrieve_contact("12") && !retrieve_contact("11")) {}
+	change_train_speed('0');
+	change_switch('7', 'G');
+	change_train_speed('4');
+	while (retrieve_contact("12") && !retrieve_contact("9")) {}
+	change_train_speed('0');
+	change_switch('6', 'R');
+change_train_direction();
+	change_train_speed('4');
+*/
